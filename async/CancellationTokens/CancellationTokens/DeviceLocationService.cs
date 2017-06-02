374820c942a03fc0b823344c008e9d8c9b260d0e
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-06-02 14:52:17 +0800

    added one more example

--- async/CancellationTokens/CancellationTokens/DeviceLocationService.cs
+++ async/CancellationTokens/CancellationTokens/DeviceLocationService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,13 +6,36 @@ namespace CancellationTokens
 {
     public class DeviceLocationService : IDeviceLocationService
     {
-        //private IGeolocatorService _geoLocatorService;
-        //public DeviceLocationService(IGeolocatorService geolocatorService)
-        //{
-        //    _geoLocatorService = geolocatorService;
-        //}
+		//private IGeolocatorService _geoLocatorService;
+		//public DeviceLocationService(IGeolocatorService geolocatorService)
+		//{
+		//    _geoLocatorService = geolocatorService;
+		//}
 
-        public async Task UpdateDeviceLocationAsync(CancellationToken cancellationToken = default(CancellationToken))
+		//public async Task UpdateDeviceLocationAsync(CancellationToken cancellationToken)
+		//{
+		//	int i = 0;
+
+		//	while (i < 10 && !cancellationToken.IsCancellationRequested)
+		//	{
+		//		Console.WriteLine($"Cancellation has not been requested after '{i}' seconds");
+
+		//		var oneSecond = 1 * 1000;
+
+		//		await Task.Delay(oneSecond);
+		//		i++;
+		//	}
+		//	if (cancellationToken.IsCancellationRequested)
+		//	{
+		//		return;
+		//	}
+
+		//	Console.WriteLine("Been running for too long bailing");
+		//	cancellationToken.ThrowIfCancellationRequested();
+		//}
+
+
+		public async Task UpdateDeviceLocationAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
             while (!cancellationToken.IsCancellationRequested)
             {
